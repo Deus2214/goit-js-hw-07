829c51ca1a443a550f86b69b2979d9517be2d258
Vitalii Gritsun <110825559+Deus2214@users.noreply.github.com>
2022-12-19 23:05:33 +0300

    Comit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,26 +1,68 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const imagesContainer = document.querySelector(".gallery");
-const imagesMarkup = createPictureGallary(galleryItems);
-imagesContainer.insertAdjacentHTML("beforeend", imagesMarkup);
-function createPictureGallary(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `<div class = "gallery__item">
-        <a class ="gallery__link" href = "${original}">
-        <img
-        class ="gallery__image"
-        src ="${preview}"
-        data-source ="${original}"
-        alt ="${description}"
-        />
-        </a>
-        </div>`;
-    })
-    .join("");
+
+const gelleryBox = document.querySelector(".gallery");
+const galleryMarkup = createMarkupGallery(galleryItems);
+
+
+gelleryBox.insertAdjacentHTML("beforeend",galleryMarkup);
+
+
+const lightbox = new SimpleLightbox(".gallery a",{captionDelay:250});
+
+
+
+
+function createMarkupGallery(array) {
+  return array
+  .map (({preview,original,description}) =>{
+     const markupItemsGallery = `<a class = "gallery__item" href="${original}">
+     <img class = "gallery__image" src = "${preview}" alt ="${description}" title ="${description}" />
+     <a/>`;
+     return markupItemsGallery;
+  })
+  .join("");
 }
-var lightbox = $('.gallery a').simpleLightbox({captionsData: 'alt',captionDelay:250,});
-conslole.log(simpleLightbox)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const imagesContainer = document.querySelector(".gallery");
+// const imagesMarkup = createPictureGallary(galleryItems);
+// imagesContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+// function createPictureGallary(galleryItems) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `<div class = "gallery__item">
+//         <a class ="gallery__link" href = "${original}">
+//         <img
+//         class ="gallery__image"
+//         src ="${preview}"
+//         data-source ="${original}"
+//         alt ="${description}"
+//         />
+//         </a>
+//         </div>`;
+//     })
+//     .join("");
+// }
+// var lightbox = $('.gallery a').simpleLightbox({captionsData: 'alt',captionDelay:250,});
+// conslole.log(simpleLightbox)
 // imagesContainer.addEventListener("click", (eve) => {
 //   eve.preventDefault();
 //   if (eve.target.nodeName !== "IMG") {
